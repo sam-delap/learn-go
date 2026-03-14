ab81262e0bc976ac89b25455b3e9101bc65af42a
Sam DeLap <sjcdelap@gmail.com>
2026-03-14 17:19:28 -0400

    Now it's correct

--- boot-dev/main.go
+++ boot-dev/main.go
@@ -6,15 +6,15 @@ func getUserMap(names []string, phoneNumbers []int) (map[string]user, error) {
 	if len(names) != len(phoneNumbers) {
 		return nil, errors.New("invalid sizes")
 	}
-	userMap := make(map[string]int)
+	userMap := make(map[string]user)
 	for i, name := range names {
 		userMap[name] = user{
-			name: name
-			phoneNumber: phoneNumbers[i]
+			name: name,
+			phoneNumber: phoneNumbers[i],
 		}
 	}
 
-	return userMap
+	return userMap, nil
 }
 
 type user struct {
