ba375dc9362a7141a5347dd39234b35dab1e7779
Sam DeLap <sjcdelap@gmail.com>
2026-03-14 15:26:09 -0400

    Accumulate number of connections

--- boot-dev/main.go
+++ boot-dev/main.go
@@ -1,43 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
-
-func printPrimes(max int) {
-	for i := 2; i < max; i++ {
-		if i == 2 {
-			fmt.Println(i)
-			continue
-		}
-		if i % 2 == 0 {
-			continue
-		}
-
-		isPrime := true
-		for j := 3; j * j <= i; j++ {
-			if i % j == 0 {
-				isPrime = false
-				continue
-			}
-		}
-		if isPrime {
-			fmt.Println(i)
-		}
+func countConnections(groupSize int) int {
+	numConnections := 0
+	for i := 1; i < groupSize; i++ {
+		numConnections += i
 	}
-}
 
-// don't edit below this line
-
-func test(max int) {
-	fmt.Printf("Primes up to %v:\n", max)
-	printPrimes(max)
-	fmt.Println("===============================================================")
-}
-
-func main() {
-	test(10)
-	test(20)
-	test(30)
+	return numConnections
 }
 
