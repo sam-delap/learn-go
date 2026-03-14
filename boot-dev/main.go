4941384ab3142339a4b9835879895ba1eec6c92c
Sam DeLap <sjcdelap@gmail.com>
2026-03-14 17:42:33 -0400

    This is concise, but not necessarily the most readable

--- boot-dev/main.go
+++ boot-dev/main.go
@@ -1,10 +1,20 @@
 package main
 
-func updateCounts(messagedUsers []string, validUsers map[string]int) {
-	for _, username := range messagedUsers {
-		val, ok := validUsers[username]; if ok {
-			validUsers[username] = val + 1
+import "unicode/utf8"
+
+func getNameCounts(names []string) map[rune]map[string]int {
+	nameCountMap := make(map[rune]map[string]int)
+	for _, name := range names {
+		firstRune, _ := utf8.DecodeRuneInString(name)
+		if _, ok := nameCountMap[firstRune]; !ok {
+			nameCountMap[firstRune] = make(map[string]int)
+			nameCountMap[firstRune][name] = 1
+		} else if val, ok := nameCountMap[firstRune][name]; !ok {
+			nameCountMap[firstRune][name] = 1
+		} else {
+			nameCountMap[firstRune][name] = val + 1
 		}
 	}
+	
+	return nameCountMap
 }
-
