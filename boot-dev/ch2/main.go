94e8e9dddbaa766cfe57ebc5dc9c041e3d944edd
Sam DeLap <sjcdelap@gmail.com>
2026-03-14 10:34:15 -0400

    Learning about defer

--- boot-dev/ch2/main.go
+++ boot-dev/ch2/main.go
@@ -1,9 +1,59 @@
 package main
 
-func reformat(message string, formatter func(string) string) string {
-	message = formatter(message)
-	message = formatter(message)
-	message = formatter(message)
-	message = "TEXTIO: " + message
-	return message
+import (
+	"fmt"
+)
+
+func bootup() {
+	defer fmt.Println("TEXTIO BOOTUP DONE")
+	// ?
+	ok := connectToDB()
+	if !ok {
+		return
+	}
+	ok = connectToPaymentProvider()
+	if !ok {
+		return
+	}
+	fmt.Println("All systems ready!")
+}
+
+// don't touch below this line
+
+var shouldConnectToDB = true
+
+func connectToDB() bool {
+	fmt.Println("Connecting to database...")
+	if shouldConnectToDB {
+		fmt.Println("Connected!")
+		return true
+	}
+	fmt.Println("Connection failed")
+	return false
+}
+
+var shouldConnectToPaymentProvider = true
+
+func connectToPaymentProvider() bool {
+	fmt.Println("Connecting to payment provider...")
+	if shouldConnectToPaymentProvider {
+		fmt.Println("Connected!")
+		return true
+	}
+	fmt.Println("Connection failed")
+	return false
+}
+
+func test(dbSuccess, paymentSuccess bool) {
+	shouldConnectToDB = dbSuccess
+	shouldConnectToPaymentProvider = paymentSuccess
+	bootup()
+	fmt.Println("====================================")
+}
+
+func main() {
+	test(true, true)
+	test(false, true)
+	test(true, false)
+	test(false, false)
 }
